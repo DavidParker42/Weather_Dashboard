e252fbbfdab4e01bea209bb19683510230625cc5
David Parker <david.m.parker92@gmail.com>
2023-03-17 18:52:32 -0400

    added function to 5 cards, initial day weather, and memory to search bar

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,14 @@ button.addEventListener("click", function(){
       return response.json();
     })
     .then(function (dataWeather){
+      // establishing array if it is in local storage
+      var storage = JSON.parse(localStorage.getItem("cities"))||[]
+      // adds new city to storage
+      storage.push(city)
+      // takes city in local storage, turns it from an array into a string
+      localStorage.setItem ("cities", JSON.stringify(storage))
+      containerEl.innerHTML=""
+      // below is main weather
         console.log(dataWeather)
         let nameEl = document.createElement("h2")
         nameEl.innerText = dataWeather.name 
@@ -44,8 +52,33 @@ button.addEventListener("click", function(){
 
       return response.json();
     })
+
+
     .then(function (fiveWeather){
         console.log(fiveWeather)
+        document.getElementById("daysWeather").innerHTML = "";
+        for (let i = 7; i < fiveWeather.list.length; i+=8) {
+          let div_el = document.createElement("div")
+          let dateEl = document.createElement("span");
+          dateEl.innerText = dataWeather.dt;
+          div_el.appendChild(dateEl);
+          let imageEl = document.createElement("img");
+          imageEl.src = `https://openweathermap.org/img/wn/${fiveWeather.list[i].weather[0].icon}@2x.png`;
+          div_el.appendChild(imageEl);
+          let tempEl = document.createElement("h2");
+          tempEl.innerText = fiveWeather.list[i].main.temp;
+          div_el.appendChild(tempEl);
+          let windEl = document.createElement("h2");
+          windEl.innerText = fiveWeather.list[i].wind.speed;
+          div_el.appendChild(windEl);
+          let humidityEl = document.createElement("h2");
+          humidityEl.innerText = fiveWeather.list[i].main.humidity;
+          div_el.appendChild(humidityEl);
+          // must come last (below)
+          document.getElementById("daysWeather").appendChild(div_el);
+        }
+
+
     })
     })
 
